4d8e25dfe91ab2fd8f372bc9137ecdbde9c4a64a
Grace <grace@Graces-MacBook-Pro.local>
2025-02-17 15:51:38 +0530

    teken samesite

--- utils/cookie.js
+++ utils/cookie.js
@@ -2,7 +2,7 @@ export const createCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly: true, // Prevent client-side JS access
         secure: process.env.NODE_ENV === 'production', // Ensure secure cookies in production
-        sameSite: 'strict', // Helps prevent CSRF attacks
+        sameSite: 'none', // Helps prevent CSRF attacks
         path: '/', // Cookie accessible across all routes
         maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
     });
