0bce0f19e08aa6ef9436ca08abc526875ca21c28
Grace <grace@Graces-MacBook-Pro.local>
2025-02-20 12:21:01 +0530

    all done

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,9 +1,20 @@
+// export const createCookie = (res, token) => {
+//     res.cookie('token', token, {
+//         httpOnly: true, // Prevent client-side JS access
+//         secure: process.env.NODE_ENV === 'production', // Secure cookies in production
+//         sameSite: process.env.NODE_ENV === 'production' ? 'lax'  : 'strict', // 'none' on production, 'strict' locally
+//         path: '/', // Cookie accessible across all routes
+//         maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+//     });
+// };
+
 export const createCookie = (res, token) => {
     res.cookie('token', token, {
-        httpOnly: true, // Prevent client-side JS access
-        secure: process.env.NODE_ENV === 'production', // Secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'lax'  : 'strict', // 'none' on production, 'strict' locally
-        path: '/', // Cookie accessible across all routes
-        maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+        httpOnly: true,  
+        secure: process.env.NODE_ENV === 'production',  
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',  
+        path: '/',
+        maxAge: 24 * 60 * 60 * 1000  
     });
 };
+
