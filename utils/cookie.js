282c55de38194874bc77ae86cd66844b37dff121
Grace <grace@Graces-MacBook-Pro.local>
2025-02-20 12:37:24 +0530

    all done

--- utils/cookie.js
+++ utils/cookie.js
@@ -12,7 +12,7 @@ export const createCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly: true,  
         secure: process.env.NODE_ENV === 'production' ? false : true, 
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',  
+        sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'none',  
         path: '/',
         maxAge: 24 * 60 * 60 * 1000  
     });
