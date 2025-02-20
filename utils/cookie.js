87fc5bf7fc63f8450a0d521ddc8bfc7622e88882
Grace <grace@Graces-MacBook-Pro.local>
2025-02-20 12:41:00 +0530

    all done

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,21 +1,21 @@
-// export const createCookie = (res, token) => {
-//     res.cookie('token', token, {
-//         httpOnly: true, // Prevent client-side JS access
-//         secure: process.env.NODE_ENV === 'production', // Secure cookies in production
-//         sameSite: process.env.NODE_ENV === 'production' ? 'lax'  : 'strict', // 'none' on production, 'strict' locally
-//         path: '/', // Cookie accessible across all routes
-//         maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-//     });
-// };
-
 export const createCookie = (res, token) => {
     res.cookie('token', token, {
-        httpOnly: true,  
-        secure: process.env.NODE_ENV === 'production' ? false : true, 
-        sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'none',  
-        path: '/',
-        maxAge: 24 * 60 * 60 * 1000  
+            sameSite: NODE_ENV === "production" ? "None" : "Lax",
+            secure: NODE_ENV === "production",
+            httpOnly: NODE_ENV === "production",
+        path: '/', // Cookie accessible across all routes
+        maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
     });
 };
+
+// export const createCookie = (res, token) => {
+//     res.cookie('token', token, {
+//         httpOnly: true,  
+//         secure: process.env.NODE_ENV === 'production' ? false : true, 
+//         sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'none',  
+//         path: '/',
+//         maxAge: 24 * 60 * 60 * 1000  
+//     });
+// };
 
 
